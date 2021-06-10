1646d88cd892b05f8ea23cf0f89c40ee23447dca
jonghyeonbyun <225whdgus@gmail.com>
2021-06-10 21:38:29 +0900

    add : firebase , google auth setting

---
+++ lib/app.dart
@@ -0,0 +1,20 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:harang/pages/home.dart';
+
+class App extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder(
+        future: Firebase.initializeApp(),
+        builder: (context, snapshot){
+      if(snapshot.hasError){
+        return Center(child: Text("Error"),);
+      }
+      if (snapshot.connectionState == ConnectionState.done){
+        return Home();
+      }
+      return CircularProgressIndicator();
+    });
+  }
+}
